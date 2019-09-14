18483f38c8e03540e66128539f410ee72d264daa
hu zhan lin <499353159@qq.com>
2019-09-14 21:50:14 +0800

    add createcomponents^C

---
+++ src/views/Mine/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="main">
+    <Header title="我的喵喵"></Header>
+    <div id="content">
+      <login></login>
+    </div>
+    <Tabbar></Tabbar>
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header";
+import Tabbar from "@/components/TabBar";
+import login from "@/components/Login";
+export default {
+  name: "Mine",
+  components: {
+    Header,
+    Tabbar,
+    login
+  }
+};
+</script>
+<style scoped>
+</style>
+
